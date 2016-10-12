8fb83e5547d1282b7df568e98d2f1514242e7cec
xyzqb <everest.qb@gmail.com>
2016-10-12 09:48:12 +0800

    利用gpio腳位模擬 風扇啟動和停止波型命令

--- src/FrequencyRun.java
+++ src/FrequencyRun.java
@@ -23,14 +23,34 @@ public class FrequencyRun {
 
 	public static void main(String[] args) throws Exception {				 		 		 	
 		FrequencyRun r=new FrequencyRun();
-		for(int i=0;i<1000;i++)
-			r.runCycle();
+		
+		
+		for(int i=0;i<10;i++){
+			r.initClock();
+			r.stop();
+		}
+			
 		r.shutdown();		
 	}
 
+	public void test() throws Exception{
+		pin.high();
+		Thread.sleep(30000);
+	}
+	
 	public void  shutdown(){
 		if(gpio!=null)
 			gpio.shutdown();
+	}
+	
+	public void initClock(){
+		long startTime = System.nanoTime();
+		long estimatedTime = 0;
+		pin.high();
+		while (estimatedTime < 339000) {
+			estimatedTime = System.nanoTime() - startTime;
+		}
+		pin.low();		
 	}
 	
 	public void runCycle() throws Exception {
@@ -66,7 +86,45 @@ public class FrequencyRun {
 		for (int i = 0; i < all.length; i++) {
 			System.out.println(i + ":" + all[i] + " :" + da[i]);
 		}
-		TimeUnit.SECONDS.sleep(5);
+		TimeUnit.SECONDS.sleep(1);
 	}
-		
+
+	public void stop() throws Exception {
+
+		long[] all = new long[72];
+		byte[] da = new byte[72];
+		for (int i = 0; i < all.length; i++) {
+			long startTime = System.nanoTime();
+			long estimatedTime = 0;
+			if (i < 54 || i > 56) {//0
+				if (i % 3 == 0) {
+					da[i] = 0;
+				} else {
+					da[i] = 1;
+				}
+			} else {
+				if (i % 3 == 2) {//1
+					da[i] = 1;
+				} else {
+					da[i] = 0;
+				}
+			}
+			if(da[i]==0){
+				pin.low();
+			}else{
+				pin.high();
+			}			
+			while (estimatedTime < 339000) {
+				estimatedTime = System.nanoTime() - startTime;
+			}
+			all[i] = estimatedTime;
+		}
+		pin.low();
+		for (int i = 0; i < all.length; i++) {
+			System.out.println(i + ":" + all[i] + " :" + da[i]);
+		}
+		TimeUnit.SECONDS.sleep(1);
+	
+	}
+	
 }
